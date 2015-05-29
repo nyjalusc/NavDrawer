68d4c7b66acd91093d9d983ce6ed73ee96e1286b
Nyjal Augustine <nyjal@box.com>
2015-05-28 20:14:10 -0700

    Lab exercise

--- app/src/main/java/com/codepath/android/navigationdrawerexercise/activities/MainActivity.java
+++ app/src/main/java/com/codepath/android/navigationdrawerexercise/activities/MainActivity.java
@@ -1,16 +1,177 @@
 package com.codepath.android.navigationdrawerexercise.activities;
 
+import android.content.res.Configuration;
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.codepath.android.navigationdrawerexercise.R;
+import com.codepath.android.navigationdrawerexercise.adapters.NavDrawerListAdapter;
+import com.codepath.android.navigationdrawerexercise.fragments.FamilyGuyFragment;
+import com.codepath.android.navigationdrawerexercise.fragments.FuturamaFragment;
+import com.codepath.android.navigationdrawerexercise.fragments.SimpsonsFragment;
+import com.codepath.android.navigationdrawerexercise.fragments.SouthParkFragment;
+import com.codepath.android.navigationdrawerexercise.models.NavDrawerItem;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    // used to store app title
+    private CharSequence mTitle;
+
+    // nav drawer title
+    private CharSequence mDrawerTitle;
+
+    // nav drawer items
+    private String[] navMenuTitles;
+
+    // nav drawer icons
+    private TypedArray navMenuIcons;
+
+    private ArrayList<NavDrawerItem> navDrawerItems;
+    private ListView mDrawerList;
+    private NavDrawerListAdapter adapter;
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Set a Toolbar to replace the ActionBar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        initView();
+        setupListeners();
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        if (savedInstanceState == null) {
+            // on first time display view for first nav item
+            displayView(0);
+        }
+    }
+
+    private void setupListeners() {
+        // Set item click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            displayView(position);
+        }
+    }
+
+    private void displayView(int position) {
+        Fragment fragment = null;
+
+        // Create a new fragment based on the item position
+        switch (position) {
+            case 0:
+                fragment = new SouthParkFragment();
+                break;
+            case 1:
+                fragment = new FamilyGuyFragment();
+                break;
+            case 2:
+                fragment = new SimpsonsFragment();
+                break;
+            case 3:
+                fragment = new FuturamaFragment();
+                break;
+            default:
+                break;
+        }
+
+        if (fragment != null) {
+            //Dynamically add a fragment into a frame container
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+            // Configure the selected item
+            mDrawerList.setItemChecked(position, true);
+            mDrawerList.setSelection(position);
+            mDrawerLayout.closeDrawer(mDrawerList);
+            // Set activity title
+            setTitle(navMenuTitles[position]);
+        } else {
+            Log.e("MainActivity", "Error in creating fragment");
+        }
+    }
+
+
+    private void initView() {
+        mTitle = mDrawerTitle = getTitle();
+
+        // load nav drawer items
+        navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items);
+
+        // nav drawer icons from resources
+        navMenuIcons = getResources().obtainTypedArray(R.array.nav_drawer_icons);
+
+        mDrawerList = (ListView) findViewById(R.id.lvDrawer);
+        navDrawerItems = new ArrayList<>();
+
+        // add nav drawer items from string array to items list
+        for (int i = 0; i < 3; i++) {
+            navDrawerItems.add(new NavDrawerItem(navMenuTitles[i], navMenuIcons.getResourceId(i, -1)));
+        }
+
+        // Recycle the typed array
+        navMenuIcons.recycle();
+
+        // set the nav drawer adapter to populate the listview
+        adapter = new NavDrawerListAdapter(getApplicationContext(), navDrawerItems);
+        mDrawerList.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // toggle nav drawer on selecting action bar app icon/title
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * When using the ActionBarDrawerToggle, you must call it during
+     * onPostCreate() and onConfigurationChanged()...
+     */
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
---
+++ app/src/main/java/com/codepath/android/navigationdrawerexercise/models/NavDrawerItem.java
@@ -0,0 +1,20 @@
+package com.codepath.android.navigationdrawerexercise.models;
+
+public class NavDrawerItem {
+    private String title;
+    private int icon;
+
+    public NavDrawerItem(String title, int icon) {
+        this.title = title;
+        this.icon = icon;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public int getIcon() {
+        return this.icon;
+    }
+}
+
